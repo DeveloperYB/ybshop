26a47a3aa9418f59f8e74217eb41e9d2dab88280
YouBeen Kim <qlqjsk@gmail.com>
2019-08-23 00:52:40 +0900

    feat(model): added model Create, Read

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const models = require('../models');
+
 const router = express.Router();
 
 router.get('/', function(req, res){
@@ -6,8 +8,26 @@ router.get('/', function(req, res){
 });
 
 router.get('/products', function(req, res){
-  const { pid } = req.query;
-  res.send(`상품 ID는 ${pid || '없음'} 입니다.`);
+  models.Products.findAll({
+
+  }).then( (products) => {
+    // DB에서 받은 products를 products변수명으로 내보냄
+    res.render( 'admin/products.html' ,{ products : products });
+  });
+});
+
+router.get('/products/write', function(req, res){
+  res.render('admin/form.html');
+});
+
+router.post('/products/write' , (req,res) => {
+  models.Products.create({
+    name : req.body.name,
+    price : req.body.price ,
+    description : req.body.description
+  }).then( () => {
+    res.redirect('/admin/products');
+  });
 });
 
 module.exports = router;
