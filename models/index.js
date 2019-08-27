7f997ca250c40a9ad30e5fdbc51d5804668b7ed4
YouBeen Kim <qlqjsk@gmail.com>
2019-08-27 19:32:22 +0900

    fix: edtied code for align

--- models/index.js
+++ models/index.js
@@ -7,15 +7,15 @@ dotenv.config(); //LOAD CONFIG
 
 const sequelize = new Sequelize( process.env.DATABASE,
 process.env.DB_USER, process.env.DB_PASSWORD,{
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    timezone: '+09:00', //한국 시간 셋팅
-    operatorsAliases: Sequelize.Op,
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  timezone: '+09:00', //한국 시간 셋팅
+  operatorsAliases: Sequelize.Op,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
 });
 
 let db = [];
