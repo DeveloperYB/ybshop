bea9b8448a26f9ec7c24dd2a554ce4e3cb2a8967
YouBeen Kim <qlqjsk@gmail.com>
2019-09-04 10:57:41 +0900

    feat(accounts): added accounts logic for check username

--- routes/accounts.js
+++ routes/accounts.js
@@ -24,7 +24,17 @@ passport.use(
     passwordField : 'password',
     passReqToCallback : true
   },
-  async ( req , username , password, done) => {
+  async (req, username, password, done) => {
+    // 아이디 조회
+    const isExistedUsername = await models.User.findOne({
+      where: {
+        username,
+      },
+      // attributes: { exclude: ['password'] }
+    });
+    if (!isExistedUsername) {
+      return done(null, false, { message: '일치하는 아이디가 존재하지 않습니다.' });
+    }
     // 조회
     const user = await models.User.findOne({
       where: {
@@ -35,7 +45,7 @@ passport.use(
     });
     // 유저에서 조회되지 않을시
     if(!user){
-      return done(null, false, { message: '일치하는 아이디 패스워드가 존재하지 않습니다.' });
+      return done(null, false, { message: '패스워드가 일치하지 않습니다.' });
     // 유저에서 조회 되면 세션등록쪽으로 데이터를 넘김
     }else{
       return done(null, user.dataValues );
@@ -47,13 +57,14 @@ router.get('/', ( _ , res) => {
   res.send('account app');
 });
 
-router.get('/join', ( _ , res) => {
-  res.render('accounts/join.html');
+router.get('/join', ( req , res) => {
+  res.render('accounts/join.html', { flashMessage : req.flash().error });
 });
 
 router.post('/join', async(req, res) => {
   const { username } = req.body;
   try {
+    // TODO: findOne vs Count 퍼포먼스 차이 비교하기
     const existedName = await models.User.count({
       where: {
         username,
