8af4f9ceaf16200d3fb5a5c71575ad2f3a755bdf
YouBeen Kim <qlqjsk@gmail.com>
2019-09-15 21:10:36 +0900

    fix: remove socket console log

--- helpers/socketConnection.js
+++ helpers/socketConnection.js
@@ -1,9 +1,6 @@
 module.exports =  (io) => {
-  const testVar = [];
   io.on('connection', (socket) => {
     socket.on('client message', (data) => {
-      testVar.push(data);
-      console.log(testVar);
       io.emit('server message', data);
     });
   });
