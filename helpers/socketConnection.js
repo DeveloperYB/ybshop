9d2dd90b522cbcb8eb19a61bdec4f4fc0a499790
YouBeen Kim <qlqjsk@gmail.com>
2019-09-15 21:09:00 +0900

    feat(username): added user name socket emit

--- helpers/socketConnection.js
+++ helpers/socketConnection.js
@@ -1,7 +1,10 @@
 module.exports =  (io) => {
+  const testVar = [];
   io.on('connection', (socket) => {
-      socket.on('client message', (data) => {
-          io.emit('server message', data.message);
-      });
+    socket.on('client message', (data) => {
+      testVar.push(data);
+      console.log(testVar);
+      io.emit('server message', data);
+    });
   });
 };
--- routes/chat.js
+++ routes/chat.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.render('chat/index.html');
+router.get('/', (req, res) => {
+  const { user } = req;
+  let displayName = '비회원';
+  if (user) displayName = user.displayname;
+  res.render('chat/index.html', { displayName });
 });
 
 module.exports = router;
